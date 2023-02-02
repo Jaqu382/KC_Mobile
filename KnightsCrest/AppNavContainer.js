1404dba876bce376e73ba6e962ff78985da40bc1
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-02-02 03:21:17 +0000

    Repaired drawer. That sucked

--- KnightsCrest/AppNavContainer.js
+++ KnightsCrest/AppNavContainer.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
-import LoginNav from './Navigation/LoginNav';
+
 import DrawerNav from './Navigation/DrawerNav';
-import HomeNav from './Navigation/HomeNav';
+
 
 export default function AppNavContainer() {
   return(
