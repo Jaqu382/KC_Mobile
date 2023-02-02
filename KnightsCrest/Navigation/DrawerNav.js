ca41a993a43a4ab131c0ba6f8375553e80513a9a
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-02-02 03:27:39 +0000

    Drawer navigation works. Thanks, I hate it

--- KnightsCrest/Navigation/DrawerNav.js
+++ KnightsCrest/Navigation/DrawerNav.js
@@ -1,33 +1,14 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeNav from "./HomeNav"
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
-  );
-}
-
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNav(){
   return (
     <Drawer.Navigator initialRouteName="Home" useLegacyImplementation={false}>
-    <Drawer.Screen name="Home" component={HomeScreen} />
-    <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-  </Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeNav} />
+    </Drawer.Navigator>
   );
 }
 
--- KnightsCrest/Navigation/HomeNav.js
+++ KnightsCrest/Navigation/HomeNav.js
@@ -6,10 +6,6 @@ import HomeScreen from "../Screens/LoginStackScreen/HomeScreen";
 import KnightsCashStack from "./KnightsCashStack.js";
 import LibraryStack from "./LibraryStack";
 
-//
-
-
-
 export default function HomeNav(){
   return (
     <Tab.Navigator initialRouteName = "Home" screenOptions={{headerShown: false} }>
