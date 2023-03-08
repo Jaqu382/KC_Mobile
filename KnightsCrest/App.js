96d922b2bf32af31c33db311a91f90578b4000ed
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-03-08 17:06:13 +0000

    Redux at the 11th hour

--- KnightsCrest/App.js
+++ KnightsCrest/App.js
@@ -1,14 +1,20 @@
-
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import ReactDOM from 'react-dom/client'
 import AppNavContainer from './AppNavContainer';
+import { Provider } from 'react-redux'
+import store from './store'
 
-
-
-function App() {
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
+    <AppNavContainer />
+  </Provider>
+)
+/*function App() {
   return(
       <AppNavContainer></AppNavContainer>
   )
 }
 
-export default App;
+export default App;*/
