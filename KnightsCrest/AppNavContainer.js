ce14264de519d58f1a8b28aa9190e54c31e0dfc9
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-03-09 04:46:17 +0000

    commit

--- KnightsCrest/AppNavContainer.js
+++ KnightsCrest/AppNavContainer.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { 
   NavigationContainer } from '@react-navigation/native';
-import LoginNav from './Navigation/LoginNav';
+import LoginNav from './navigation/LoginNav';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function AppNavContainer() {
