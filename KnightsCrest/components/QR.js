e0be0a4b15f723f794d2ac6d40e9c72527918866
Juan Aquino <97471355+Jaqu382@users.noreply.github.com>
2023-04-18 22:19:19 +0000

    fixed source uri thing

--- KnightsCrest/components/QR.js
+++ KnightsCrest/components/QR.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import QRCode from 'react-native-qrcode-svg';
+import { useSelector } from 'react-redux';  
 
-export default function QR (props){
+export default function QR (){
 
-    const {user} = props
+    const user = useSelector((state) => state.user);
     const userString = JSON.stringify(user);
-    return(     
-            <QRCode
-            size={150}
-            value = {user}
-            color="black"
-            backgroundColor="white"
-            />
-        )
-    }
+
+    return (
+    <QRCode
+      size={150}
+      value={userString}
+      color="black"
+      backgroundColor="white"
+    />
+  );
+}
